79f6dfc581644a08c5bdf0c2714db3b23332fca4
AbdoYasser74 <130872766+AbdoYasser74@users.noreply.github.com>
2024-05-03 15:42:35 +0300

    Initial commit

--- app/src/main/java/com/abdroid/egylens/presentation/details/DetailsScreen.kt
+++ app/src/main/java/com/abdroid/egylens/presentation/details/DetailsScreen.kt
@@ -1,7 +1,5 @@
 package com.abdroid.egylens.presentation.details
 
-import android.widget.Toast
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -26,33 +24,27 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.abdroid.egylens.R
 import com.abdroid.egylens.domain.model.Statue
 import com.abdroid.egylens.ui.theme.notoFont
-import com.abdroid.egylens.util.UIComponent
 
 
 @Composable
